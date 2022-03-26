3c4c4e64c888f801306b6e5ca4a9c25e796557fc
ZivKa <zivkalderon95@gmail.com>
2022-03-26 21:09:59 +0300

    comments

--- static/index.js
+++ static/index.js
@@ -2,7 +2,10 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 (async () => {
+	// Network request to get the website
 	const { data } = await axios.get("https://www.nodetlv.com/");
+
+	// Extract the data using cheerio - supports css selector/xpath and more
 	const $ = cheerio.load(data);
 	console.log(`The website title is **** ${$("title").text()} ****`);
 })();
