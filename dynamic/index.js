fd28ccb84427906ae0ca0b4f9f56bac9879c2209
ZivKa <zivkalderon95@gmail.com>
2022-03-26 21:09:40 +0300

    comments

--- dynamic/index.js
+++ dynamic/index.js
@@ -2,12 +2,19 @@ const puppeteer = require('puppeteer');
 const cheerio = require("cheerio");
 
 (async () => {
+	// Open the bowser
 	const browser = await puppeteer.launch({headless:false});
+	// Open new tab
 	const page = await browser.newPage();
+	// Network request to get the website
 	await page.goto('https://www.nodetlv.com/');
+
+	// Extract the data
 	const title = await page.evaluate(() => {
 		return document.title
 	  });
 	console.log(`The website title is **** ${title} ****`);
+
+	// Close the browser
 	browser.close();
 })();
